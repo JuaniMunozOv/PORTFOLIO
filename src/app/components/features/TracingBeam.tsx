5028880627fba5f99fd0dd6a53a423394d30e7aa
Juan Ignacio Muñoz <108429659+JuaniMunozOv@users.noreply.github.com>
2024-04-19 21:51:11 -0300

    Update TracingBeam.tsx

--- src/app/components/features/TracingBeam.tsx
+++ src/app/components/features/TracingBeam.tsx
@@ -44,28 +44,19 @@ const dummyContent = [
     description: (
       <>
         <p>
-          My name is Juan Ignacio Muñoz, I am a software engineering student and a programmer
-          with knowledge in different programming languages such as Python, C++, Html, Css and React among others.
-          This is a small demo of my work using Next.js and Tailwindcss.
-          But my forte and my growth goals are based on the development of geographic information systems software.
-          geographic information systems (GIS) software. During 2020 and 2021 I've dedicated myself to
-          complement Python with geospatial libraries such as Geopandas, Shapely and to complement with PyQgis the
-          through PyQgis the development of plugins for the Qgis desktop software.
-          Qgis is an open source software that allows the management of geographic information and the creation of maps.
-          the creation of maps. The development of cartography and the creation of thematic maps is one of my passions.
-          one of my passions and being able to execute it with satellite images is a challenge that I carry out with a lot of dedication to achieve
-          with a lot of dedication to achieve quality results using satellites such as Landsat ,
-          Sentinel 1 | 2 | 3 and others.
-
+          My name is Juan Ignacio Muñoz Ovalles, I am a software engineering student and programmer with knowledge in different programming languages
+          such as Python, C++, R, among others.This is a small demonstration of my work using Next.js and Tailwindcss, but my forte and my growth goals
+          are based on the development of geographic information systems (GIS) software.
+          During 2020 and 2021 I dedicated myself to complement Python with geospatial libraries such as Geopandas, Shapely and complement with PyQgis
+          the development of plugins for the desktop software Qgis, 
+          This is an open source software that allows the management of geographic information and the creation of maps along with the development of cartography
+          which is one of my passions and to be able to run it with satellite images is a challenge that I carry out with great dedication 
+          to achieve quality results using satellites such as Landsat, Sentinel 1 | 2 | 3 and others.
         </p>
         <p>
-          My learning objectives were growing and reinforcing during the last 4 years, where through courses, tutorials and personal projects
-          where through courses, tutorials and personal projects I was able to acquire knowledge in software development
-          software development and geographic information systems (GIS). Applying cartography
-          in different works, in order to develop slope calculations, contour lines, flood plains,
-          photogrammetry using and piloting drones for taking aerial photographs and creating orthophotos.
-          In this way and complementing Qgis I was able to carry out the creation of thematic maps, as well as 3D views of terrains.
-          as well as 3D views of terrains and study surfaces.
+          My learning objectives were growing and reinforcing during the last 4 years where I was able to apply cartography in different works
+          in order to develop slope calculations, contour lines, flood plains, photogrammetry using and piloting drones for taking aerial photographs and creating orthophotos.
+          In this way and complementing Qgis I was able to create thematic maps, as well as 3D views of terrain and study surfaces.
         </p>
       </>
     ),
